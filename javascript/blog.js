5357bbc1ea85b91db4ed1c8ca88fc7c1beb7abe2
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-01-26 13:59:08 +0100

    Skapat metoder för att spara och hämta blogggar från localStorage. Kommenterat kod för dokumentation.

--- javascript/blog.js
+++ javascript/blog.js
@@ -1,13 +1,18 @@
+/**
+ * Represents a blog
+ * @param {string} title - Title of the blog
+ */
 class Blog {
 	constructor(title) {
 		this.title = title;
 
 		this.blogs = [];
 		this.blogId = 1;
+		this.getBlogsFromLocalStorage();
 	}
 
 	/**
-	 * Creates a new blogpost and saves it to the blogs array.
+	 * Creates a new blogpost and saves it to the blogs array. Also saves to localStorage.
 	 * @param  {string} heading - Heading of the blogpost
 	 * @param  {URL} picture - Title picture URL
 	 * @param  {string} blogText - Content of the blogpost
@@ -16,6 +21,7 @@ class Blog {
 		//TODO: Ha detta?
 		if (heading.length < 2 || picture.length < 3 || blogText.length < 10)
 			return;
+
 		const today = new Date();
 		let newBlog = {
 			id: this.blogId,
@@ -26,6 +32,7 @@ class Blog {
 		};
 		this.blogId++;
 		this.blogs.push(newBlog);
+		this.saveBlogsToLocalStorage();
 	}
 	/**
 	 * Removes a blog post from the array
@@ -35,8 +42,31 @@ class Blog {
 		for (let i = 0; i < this.blogs.length; i++) {
 			if (this.blogs[i].id === id) {
 				this.blogs.splice(i, 1);
+				this.saveBlogsToLocalStorage();
 				return;
 			}
 		}
+	}
+	/**
+	 * Saves the blog array to the localStorage
+	 */
+	saveBlogsToLocalStorage() {
+		localStorage.clear();
+		localStorage.setItem("blogs", JSON.stringify(this.blogs));
+		localStorage.setItem("blogId", this.blogId.toString());
+		console.log(localStorage);
+	}
+	/**
+	 * Gets the blog list from localStorage if there is any, otherwise return
+	 */
+	getBlogsFromLocalStorage() {
+		if (localStorage.length === 0) return;
+		this.blogs.splice(0, this.blogs.length);
+
+		let storedBlogs = JSON.parse(localStorage.getItem("blogs"));
+		storedBlogs.forEach((blog) => {
+			this.blogs.push(blog);
+		});
+		this.blogId = +localStorage.getItem("blogId");
 	}
 }
